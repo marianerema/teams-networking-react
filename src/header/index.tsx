5a1d690f5de011eb2f2856fa5163f4e3dcaa97ee
marianerema <116829661+marianerema@users.noreply.github.com>
2023-05-18 21:29:38 +0300

    move AppHeader in header/index.tsx

---
+++ src/header/index.tsx
@@ -0,0 +1,30 @@
+import selfie from "../marian.jpeg";
+import MainMenu from "../menu/MainMenu";
+
+export default function AppHeader() {
+  return (
+    <header>
+      <div id="header-wrapper">
+        <div>
+          <img id="avatar" src={selfie} alt="Erema" width="150px" />
+        </div>
+        <div>
+          <h1>Erema Marian</h1>
+          <h2 id="job-title">
+            Teacher of physical education and sports & Student at
+            <a href="https://fasttrackit.org/" target="_blank">
+              FastTrackIT
+            </a>{" "}
+            & Trainer at
+          </h2>
+        </div>
+        <div>
+          <a target="_blank" href="https://www.facebook.com/sportiland">
+            <img id="sportiland" src="SportiLand-COLOR.png" alt="sportiland" height="85px" className="link" />
+          </a>
+        </div>
+      </div>
+      <MainMenu />
+    </header>
+  );
+}
